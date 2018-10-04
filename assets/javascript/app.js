35ac75f8c22a0fd58aeb667dc421fe3459541fa1
Kyle Rosenmeyer <kylerosenmeyer@gmail.com>
2018-10-03 21:12:33 -0400

    map api functionality working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,25 +2,8 @@
 //Hacker News API Key: cf8f9bf8ef354b0687c21810053ada1d
 
 // $.ajax({
-//     url: "http://api.worldbank.org/v2/countries/all/indicators/SP.POP.TOTL?page=2",
-//     METHOD: "GET"
-// }).then( function(snap){
-//     console.log(snap)
+//     url: "https://places.cit.api.here.com/places/v1/discover/search?app_id=qA8H4eSKAGPuJ30pTryC&app_code=e7_nFDb6gHJ1Hvp40oOtSQ&at=52.531,13.3843&q=Brandenburg+Gate",
+//     method: "GET"
+// }).then( function(response) {
+//     console.log(response)
 // })
-
-var platform = new H.service.Platform({
-    'app_id': 'qA8H4eSKAGPuJ30pTryC',
-    'app_code': 'e7_nFDb6gHJ1Hvp40oOtSQ'
-  });
-
-  // Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var map = new H.Map(
-  document.getElementById('mapContainer'),
-  defaultLayers.normal.map,
-  {
-    zoom: 10,
-    center: { lat: 52.5, lng: 13.4 }
-  });
