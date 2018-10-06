ee23944dfd9a50ebc265800854dbcc980fadc639
AudenBlack@users.noreply.github.com <41378366+AudenBlack@users.noreply.github.com>
2018-10-06 11:32:21 -0400

    updated index to grid design

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //Google Search API Key: AIzaSyDc8BAjqmHGQvYaYLW_bg_AxP986qIkdks
 //Hacker News API Key: cf8f9bf8ef354b0687c21810053ada1d
 
-// $.ajax({
+ $.ajax({
 //     url: "https://places.cit.api.here.com/places/v1/discover/search?app_id=qA8H4eSKAGPuJ30pTryC&app_code=e7_nFDb6gHJ1Hvp40oOtSQ&at=52.531,13.3843&q=Brandenburg+Gate",
 //     method: "GET"
 // }).then( function(response) {
